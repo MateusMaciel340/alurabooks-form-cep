969eb6b17dace0ecd33782f3cf0bdbe15ee689ce
mateusmaciel340 <mateusmaciel340@gmail.com>
2023-05-14 13:51:49 -0300

    inicial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log("Enviando mensagem!");
+
+function mandarMensagem() {
+    console.log("Tudo bem?");
+    console.log("Vou te mandar uma solicitação!");
+    console.log("Solicitação recebida!");
+}
+
+setTimeout(mandarMensagem, 5000);
+console.log("Tchau!");
